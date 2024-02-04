b59e6bdd4b7856bc5c110832d0f5af84721618fb
Daniel Moura <daniel.lucio.moura@gmail.com>
2024-02-04 19:23:47 -0300

    Initial Commit

---
+++ Barcode Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode Scanner
+//
+//  Created by Daniel Moura on 04/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
